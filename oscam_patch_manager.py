2d5ec8d3e9daeda93205efbf6f937493c5070149
speedy005 <67871007+speedy005@users.noreply.github.com>
2026-02-01 12:37:18 +0100

    Update oscam_patch_manager.py

--- oscam_patch_manager.py
+++ oscam_patch_manager.py
@@ -74,7 +74,7 @@ now = QDateTime.currentDateTime()
 time_str = now.toString("HH:mm:ss")
 date_str = now.toString("dd.MM.yyyy")
 # ===================== APP CONFIG =====================
-APP_VERSION = "2.2.2"
+APP_VERSION = "2.2.3"
 # Basis-Verzeichnis des Scripts (absoluter Pfad)
 PLUGIN_DIR = os.path.dirname(os.path.abspath(__file__))
 
